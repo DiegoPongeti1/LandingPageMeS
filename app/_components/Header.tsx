b80f790f36175ba84d4c7c8c0f1f20a49707b91c
Diego Pongeti <diego-p43@hotmail.com>
2026-02-10 15:36:46 -0300

    Hover nas infos

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,21 +1,24 @@
 
 import Image from 'next/image'
+
+
 export function Header() {
+
     return (
         <header className="bg-[#be0505e8] w-full border-b border-orange-100/50">
             <div className='flex flex-col justify-center items-center py-10 '>
                 <h1>
-                    <Image 
-                    src="/Micro&Soft Informática.png"
-                    alt="Logo"
-                    width="300"
-                    height="300"
-                    className='object-contain border-3 rounded-lg  py-5 px-5'
+                    <Image
+                        src="/Micro&Soft Informática.png"
+                        alt="Logo"
+                        width="300"
+                        height="300"
+                        className='object-contain border-3 rounded-lg  py-5 px-5'
                     />
                 </h1>
 
                 <p className='text-center font-extrabold text-3xl text-gray-600 tracking-tight'>
-                    
+
                 </p>
 
             </div>
--- app/_components/Infos.tsx
+++ app/_components/Infos.tsx
@@ -2,37 +2,37 @@
 
 import { WhatsappLogo, Phone, EnvelopeSimple, MapPin } from "@phosphor-icons/react"
 
- 
+
 export function Infos() {
     return (
         <section className="py-20 bg-gray-100">
             <div className="container mx-auto px-4">
-               
+
                 <div className="max-w-2xl mx-auto bg-white border border-gray-200 shadow-xl rounded-2xl p-8 md:p-12">
-                    
+
                     <div className='flex flex-col items-center justify-center text-center'>
                         <h2 className="text-2xl font-bold text-gray-900 mb-6">Informações de Contato</h2>
-                        
-                        
+
+
                         <div className="space-y-4 mb-8 text-gray-600 w-full">
                             <div className="flex items-center justify-center gap-3 text-black">
                                 <Phone size={20} className="text-[#800000]" />
-                                <a href="tel:+551637240011">(16) 3724-0011</a>
+                                <a className="hover:text-[#800000] transition-colors" href="tel:+551637240011">(16) 3724-0011</a>
                             </div>
-                            
+
                             <div className="flex items-center justify-center gap-3 text-black">
                                 <EnvelopeSimple size={20} className="text-[#800000]" />
-                                <a href="mailto:falecom@microesoft.com.br" >falecom@microesoft.com.br</a>
+                                <a className="hover:text-[#800000] transition-colors" href="mailto:falecom@microesoft.com.br" >falecom@microesoft.com.br</a>
                             </div>
-                            
+
                             <div className="flex items-start justify-center gap-1 text-black">
                                 <MapPin size={20} className="text-[#800000] shrink-0 mt-1" />
-                                <a href="https://maps.app.goo.gl/NnjrrG53YkLbvZXX8 " target="_blank" className="max-w-xs leading-relaxed">R. Augusto Marques, 1711 - Vila Flores, Franca - SP</a>
+                                <a className="hover:text-[#800000] transition-colors" href="https://maps.app.goo.gl/NnjrrG53YkLbvZXX8 " target="_blank" className="max-w-xs leading-relaxed">R. Augusto Marques, 1711 - Vila Flores, Franca - SP</a>
                             </div>
                         </div>
 
-                        
-                        <a 
+
+                        <a
                             href="https://wa.me/5516992342975?text=Olá! Gostaria de mais informações sobre os serviços da Micro&Soft."
                             target="_blank"
                             rel="noopener noreferrer"
